ebd605a1d2842df5fd45eae9fec577c4b89e8b5a
gestagene <gestagenebrian@gmail.com>
2026-07-10 22:53:36 +0800

    feat: add multer middleware for image uploads

--- src/features/posts/posts.routes.ts
+++ src/features/posts/posts.routes.ts
@@ -6,14 +6,19 @@ import {
   deletePost,
 } from "../posts/posts.controller.js";
 import { requireAuth } from "../../middleware/auth.middleware.js";
-
+import multer from "multer";
 import express from "express";
+
 const router = express.Router();
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: { fileSize: 5 * 1024 * 1024 },
+});
 
 router.get("/", getPosts);
 router.get("/:id", getPost);
 
-router.post("/", requireAuth, createPost);
+router.post("/", requireAuth, upload.single("image"), createPost);
 router.put("/:id", requireAuth, updatePost);
 router.delete("/:id", requireAuth, deletePost);
 
